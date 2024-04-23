06173e59226f03e07827ea6580432ca3fbcd6349
Fernando De La Madrid <fernando.delamadrid@checkr.com>
2024-04-23 13:01:47 -0700

    add theme + ui library

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,13 @@
-import './global.css';
+// import './global.css';
+import './globals.css';
 import type { Metadata } from 'next';
 import { GeistSans } from 'geist/font/sans';
 import { GeistMono } from 'geist/font/mono';
 import { Navbar } from './components/nav';
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
+// TODO: Aqui me quede
+import { ThemeProvider } from "./components/theme-provider";
 import { SandpackCSS } from './blog/[slug]/sandpack';
 
 export const metadata: Metadata = {
@@ -54,7 +57,6 @@ export default function RootLayout({
     <html
       lang="en"
       className={cx(
-        'text-black bg-white dark:text-white dark:bg-[#111010]',
         GeistSans.variable,
         GeistMono.variable
       )}
@@ -63,12 +65,19 @@ export default function RootLayout({
         <SandpackCSS />
       </head>
       <body className="antialiased max-w-2xl mb-40 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto">
-        <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
-          <Navbar />
-          {children}
-          <Analytics />
-          <SpeedInsights />
-        </main>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
+            <Navbar />
+            {children}
+            <Analytics />
+            <SpeedInsights />
+          </main>
+        </ThemeProvider>
       </body>
     </html>
   );
